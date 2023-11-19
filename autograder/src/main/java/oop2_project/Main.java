7c7d9c02e29b3db1b0ba2d715056df95b7fd9669
Jerrelle Johnson <jerrellejohnson@Jerrelles-MacBook-Air.local>
2023-11-19 10:46:41 -0400

    renamed so of the testdata

--- autograder/src/main/java/oop2_project/Main.java
+++ autograder/src/main/java/oop2_project/Main.java
@@ -8,7 +8,7 @@ public class Main {
     {
         Facade grade = new AutoGradeFacade();
 
-        String zipFile = "/Users/jerrellejohnson/Desktop/tesing/submissions5.zip";
+        String zipFile = "testData/Submission1.zip";
        
         Scanner cin = new Scanner(System.in);
         // System.out.print("Enter the filepath of the zipped submissions: ");
